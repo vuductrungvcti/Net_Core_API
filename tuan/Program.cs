95a9034a388a9a71bb3989dfe8387866069dfcf0
vuductrungvcti <vuductrungvcti.gmail.com>
2023-08-07 16:03:57 +0700

    move code

--- tuan/Program.cs
+++ tuan/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection.Emit;
+using System.Data;
+
+using System;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, dsfjlkasjfl;jasldfj!");
+//in ra màn hình: 6
+Console.WriteLine(6);
+//in ra màn hình: a = 9
+int a = 9; //khai bao biến số a kiểu int và gán giá trị = 9
+Console.WriteLine("a = " + a);
+//in ra màn hình: a + b = 12
+int b = 3;
+Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
+String c="Bùi "; String d="Anh "; 
+Console.WriteLine("Hello, Nhap so lieu: ");
+String e = Console.ReadLine();
+Console.WriteLine(c+d+e);
+
+
